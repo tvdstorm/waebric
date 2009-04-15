865d1a9e1d0d56484e6362251232d06ddfb1dde6
mwullink <mwullink@370d2b24-1e00-11de-8120-8d224146dbbc>
2009-04-15 08:49:14 +0000

    test voor de demo

--- implementations/java/jparsec/parser/WaebrickParser/src/com/uva/se/app/Main.java
+++ implementations/java/jparsec/parser/WaebrickParser/src/com/uva/se/app/Main.java
@@ -1,33 +1,55 @@
 package com.uva.se.app;
-import java.util.Arrays;
-import java.util.List;
-
 import org.codehaus.jparsec.Parser;
 import org.codehaus.jparsec.Parsers;
 import org.codehaus.jparsec.Scanners;
 import org.codehaus.jparsec.Terminals;
+import org.codehaus.jparsec.functors.Map;
 
 
 public class Main {
 
-	/**
-	 * @param args
-	 */
+
+	
+	private static final String[] OPERATORS = {
+		  "&&", "||" // and all other operators.
+		};
+
+	
+	private static final String[] KEYWORDS = {
+		  "module", "import", "site", "def", "echo", "each" // all other keywords
+		};
+	
+	static final Parser<Void> IGNORED =
+		   Parsers.or(Scanners.JAVA_LINE_COMMENT, Scanners.JAVA_BLOCK_COMMENT, Scanners.WHITESPACES).skipMany();
+	
+	private static final Terminals TERMINALS = Terminals.caseSensitive(OPERATORS, KEYWORDS);
+
+	
+	static final Parser<?> TOKENIZER = TERMINALS.tokenizer();
+	
+	  static final Parser<String> TOKENS = Parsers.ANY_TOKEN.map(new Map<Object, String>() {
+	      public String map(Object s) {
+	    	System.out.println("TOKEN FOUND == " + s.toString());
+	        return "test";
+	      }
+	    });
+
+	public static final Parser<String> PARSER = TOKENS.from(TOKENIZER, IGNORED);
+
+	
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Main m = new Main();
+		m.doiets();
 
 	}
 	
-	public int bla(){
-		Terminals operators = Terminals.operators(","); // only one operator supported so far
-		Parser<?> integerTokenizer = Terminals.IntegerLiteral.TOKENIZER;
-		Parser<String> integerSyntacticParser = Terminals.IntegerLiteral.PARSER;
-		Parser<?> ignored = Parsers.or(Scanners.JAVA_BLOCK_COMMENT, Scanners.WHITESPACES);
-		Parser<?> tokenizer = Parsers.or(operators.tokenizer(), integerTokenizer); // tokenizes the operators and integer
-		Parser<List<String>> integers = integerSyntacticParser.sepBy(operators.token(","))
-		    .from(tokenizer, ignored.skipMany());
-		//assert(Arrays.asList("1", "2", "3"), integers.parse("1, /*this is comment*/2, 3");
-		return 1;
+	public void doiets(){
+		System.out.println("Start parser");
+		PARSER.parse("module import echo");
+		System.out.println("End parser");
+
 	}
 
 	
