e4fb5df9ae411f513b774e97dd937334b1365b97
mwullink <mwullink@370d2b24-1e00-11de-8120-8d224146dbbc>
2009-05-27 14:06:03 +0000

    addded comments and some refactoring of resource loading code

--- implementations/java/jparsec/parser/WaebrickParser/src/com/uva/se/wparse/resource/Resources.java
+++ implementations/java/jparsec/parser/WaebrickParser/src/com/uva/se/wparse/resource/Resources.java
@@ -1,14 +1,32 @@
+/*
+ * File			: Resources.java
+ * Project		: WaebrickParser
+ * 				: Waebrick Parser, practicum opdracht Software Construction
+ * 
+ * Author		: M. Wullink, L. Vinke, M. v.d. Laar
+ * 
+ * Description	: Enum which contains elements for all the items items of the messages properties file.
+ * 
+ * 
+ * Change history
+ * -----------------------------------------------------------
+ * Date			Change				 
+ * -----------------------------------------------------------
+ * 07-05-2009	Initial version.
+ * 
+ * 
+ */
 package com.uva.se.wparse.resource;
 
 public enum Resources {
-	MAIN_ERROR_NOARGS("main.error.noargs"),
+	
 	GUI_BUTTON_LOAD("gui.button.load"),
 	GUI_TITLE("gui.title"),
 	
 	
-	PARSER_ERROR("parser.error"),
-	FILE_ERROR("file.error"),
-	
+	ERROR_PARSER("error.parser"),
+	ERROR_FILE("error.file"),
+	ERROR_MAIN_NOARGS("error.main.noargs"),
 	ERROR_DIALOG_TITLE("error.dialog.title"),
 	;
 
@@ -16,15 +34,26 @@ public enum Resources {
 
 	private ResourceLoader resourceLoader = ResourceLoader.getInstance();
 
+	/**
+	 * Resources can only be defined by this enum itself 
+	 * @param key the value of the key
+	 */
 	private Resources(final String key) {
 		this.key = key;
 	}
 
+	/**
+	 * Get the name of the enum as String
+	 */
 	@Override
 	public String toString() {
 		return key;
 	}
 
+	/**
+	 * Get the value of the enum
+	 * @return value actual value of the message this enum links to
+	 */
 	public String getResource() {
 		return resourceLoader.getResource(key);
 	}
