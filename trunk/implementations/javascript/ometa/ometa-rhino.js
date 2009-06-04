bcf509ea2c7cc5d58c96990a9a3cd6b3afb8abe9
djnickolas1@gmail.com <djnickolas1@gmail.com@370d2b24-1e00-11de-8120-8d224146dbbc>
2009-06-04 15:44:51 +0000

    Constructing Abstract Syntax Tree in OMeta:  - Modules  - Sites  - Mappings  - Paths  - Expressions  - Designator  - Attributes  - Arguments
    Separate files for parser and interpreter in OMeta + changed file extensions
    Learning the OMeta interpreter syntax (online)

--- trunk/implementations/javascript/ometa/ometa-rhino.js
+++ trunk/implementations/javascript/ometa/ometa-rhino.js
@@ -11,47 +11,79 @@ load("bs-ometa-js-compiler.js")
 alert = print
 
 translateCode = function(s) {
-  var translationError = function(m, i) { alert("Translation error - please tell Alex about this!"); throw fail },
-      tree             = BSOMetaJSParser.matchAll(s, "topLevel", undefined, function(m, i) { throw fail.delegated({errorPos: i}) })
-  return BSOMetaJSTranslator.match(tree, "trans", undefined, translationError)
+	var translationError = function(m, i) { alert("Translation error - please tell Alex about this!"); throw fail },
+    tree = BSOMetaJSParser.matchAll(s, "topLevel", undefined, function(m, i) { throw fail.delegated({errorPos: i}) })
+  	return BSOMetaJSTranslator.match(tree, "trans", undefined, translationError)
 }
 
-getGrammar = function() { 
-	fis = new FileInputStream('grammar.txt');  
+getParser = function() {
+	fis = new FileInputStream('parser.ometa');
 	bis = new BufferedInputStream(fis);
 	dis = new DataInputStream(bis);
-	
-	grammar = '';
+
+	parser = '';
 	while(dis.available() != 0){
-		grammar += dis.readLine() + '\n'; 
+		parser += dis.readLine() + '\n';
 	}
 	fis.close();
 	bis.close();
 	dis.close();
-	return grammar;
-} 
+	return parser;
+}
 
-getProgram = function() { 
-	fis = new FileInputStream('program.txt');  
-	bis = new BufferedInputStream(fis);
-	dis = new DataInputStream(bis);
-	
-	program = '';
-	while(dis.available() != 0){
-		program += dis.readLine() + '\n'; 
+getInterpreter = function() {
+	fis = new FileInputStream('interpreter.ometa');
+    bis = new BufferedInputStream(fis);
+    dis = new DataInputStream(bis);
+
+    interpreter = '';
+    while(dis.available() != 0){
+    	interpreter += dis.readLine() + '\n';
+    }
+    fis.close();
+    bis.close();
+    dis.close();
+    return interpreter;
+}
+
+getProgram = function() {
+    fis = new FileInputStream('interpreter.wae');
+    bis = new BufferedInputStream(fis);
+    dis = new DataInputStream(bis);
+
+    program = '';
+    while(dis.available() != 0){
+    	program += dis.readLine() + '\n';
 	}
 	fis.close();
 	bis.close();
 	dis.close();
 	return program;
-} 
-
-getSource = function(){
-	return getGrammar() + '; alert(Waebric.matchAll(getProgram(), "Modules"))'
 }
 
-compile = function() { 
-	return eval(translateCode(getSource()))
+getSource = function(){
+	program = getProgram();
+
+	source 	= getParser() + ';'
+	        + 'tree = WaebricParser.matchAll(program, "Modules");'
+	        + 'alert("\n");'
+	        + 'alert("\n");'
+	        + 'alert("\n");'
+	        + 'alert("----------------------------------------------------------------------------------------------------");'
+	        + 'alert(tree);'
+	        + 'alert("----------------------------------------------------------------------------------------------------");'
+	        + 'alert("\n");'
+	        + 'alert("\n");'
+	        + 'alert("\n");'
+			+ getInterpreter() + ';'
+			// + 'html_output = WaebricInterpreter.match(tree, "interp");'
+			//+ 'alert(html_output);'
+
+	return source;
+}
+
+compile = function() {
+    return eval(translateCode(getSource()))
 }
 
 compile()
