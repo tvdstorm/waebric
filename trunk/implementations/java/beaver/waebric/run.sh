d7f654b650fd6c70df8520316f95239f350b0e40
allarddijk@hotmail.com <allarddijk@hotmail.com@370d2b24-1e00-11de-8120-8d224146dbbc>
2009-06-07 10:37:02 +0000

    de vorige versie werkte niet op Mac/Linux

--- trunk/implementations/java/beaver/waebric/run.sh
+++ trunk/implementations/java/beaver/waebric/run.sh
@@ -1,8 +1,3 @@
-# run.sh
-#
-# Genereert een Waebric AST op basis van input-bestand <filenaam>.wae
-# en zet deze in output-bestand <filenaam>.txt
-#
-java -classpath ../lib/beaver.jar:.:.. waebric/Main test/input/$1.wae > test/output/$1.txt
-
-
+java -classpath ../lib/beaver.jar:.:.. waebric/Main $1.wae > test/output/$1.txt
+cat test/output/$1.txt
+echo
