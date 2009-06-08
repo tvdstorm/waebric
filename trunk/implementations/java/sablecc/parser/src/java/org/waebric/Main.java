0285865da0ace9d6ccf31c2548cd53f3f535ee18
sander.moelands <sander.moelands@370d2b24-1e00-11de-8120-8d224146dbbc>
2009-06-08 14:17:26 +0000

    - Full waebric parsing support - AST output generated equal to asf+sdf output

--- trunk/implementations/java/sablecc/parser/src/java/org/waebric/Main.java
+++ trunk/implementations/java/sablecc/parser/src/java/org/waebric/Main.java
@@ -1,8 +1,11 @@
 package org.waebric;
 
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.OutputStream;
 import java.io.PushbackReader;
 
 import org.sablecc.grammar.waebric.lexer.Lexer;
@@ -14,7 +17,7 @@ import org.waebric.adapter.TreeWalker;
 public class Main {
 	public static void main (String args[])
 	{
-		if (args.length < 1 || !new File(args[0]).exists())
+		if (args.length < 2 || !new File(args[0]).exists())
 			System.out.println("Please specify a valid waebric file to parse...");
 		else
 		{
@@ -23,13 +26,27 @@ public class Main {
 		        
 		        Parser p = new Parser (l);
 		        Start start = p.parse ();
-		        System.out.println (start.toString());
+//		        System.out.println (start.toString());
 	
 		        TreeWalker walker = new TreeWalker();
 		        start.apply(walker);
-	
-		        ASTDisplay ad = new ASTDisplay ();
-		        start.apply (ad);
+		        
+		        OutputStream out = null;
+		        try {
+    		        	File f = new File(args[1]);
+    		        	out = new BufferedOutputStream(new FileOutputStream(f));
+    		        	out.write(walker.getAstTree().getBytes());
+		        }
+		        catch (Exception e) {
+		            e.printStackTrace();
+		        }
+		        finally {
+		            if (out != null)
+		        	out.close();
+		        }
+		        
+//		        ASTDisplay ad = new ASTDisplay ();
+//		        start.apply (ad);
 	
 		      } catch (Exception e) {
 		        e.printStackTrace();
