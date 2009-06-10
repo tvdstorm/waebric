1274ede02812097aefe434cd722c3c239fb8eb73
lvinke@hotmail.com <lvinke@hotmail.com@370d2b24-1e00-11de-8120-8d224146dbbc>
2009-06-10 13:56:09 +0000

    Refactorings and documentation.

--- implementations/java/jparsec/parser/WaebrickParser/src/com/uva/se/wparse/output/transformer/OutputTransformer.java
+++ implementations/java/jparsec/parser/WaebrickParser/src/com/uva/se/wparse/output/transformer/OutputTransformer.java
@@ -21,8 +21,16 @@ package com.uva.se.wparse.output.transformer;
 import com.uva.se.wparse.model.common.WabrickParseTree;
 import com.uva.se.wparse.output.WaebricParsetreeTransformer;
 
+/**
+ * This class transforms the Weabric tree to the expected output. 
+ */
 public class OutputTransformer implements WaebricParsetreeTransformer{
 	
+	/**
+	 * Transforms the weabric ParseTree to the expected output. 
+	 * @param waebricParseTree The waebric parse tree. 
+	 * @return The expected output. 
+	 */
 	public String transform(WabrickParseTree waebricParseTree){
 		
 		return waebricParseTree.getTransformerOutput();
--- implementations/java/jparsec/parser/WaebrickParser/src/com/uva/se/wparse/resource/ResourceLoader.java
+++ implementations/java/jparsec/parser/WaebrickParser/src/com/uva/se/wparse/resource/ResourceLoader.java
@@ -21,35 +21,51 @@ package com.uva.se.wparse.resource;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
+/**
+ * This class can load different resources out of a Java resouce file. 
+ */
 public class ResourceLoader {
-	
+
+	/**
+	 * All the different messages which are stored in the resource.
+	 */
 	private ResourceBundle messages = null;
-	
+
+	/**
+	 * The loader of the resource.
+	 */
 	private static ResourceLoader resourceLoader = null;
-	
-	private ResourceLoader(){
+
+	/**
+	 * Constructs the resource loaded. 
+	 */
+	private ResourceLoader() {
 		String packagePath = getClass().getPackage().getName();
 		packagePath = packagePath.replace(".", "/");
-		messages = ResourceBundle.getBundle(packagePath + "/messages", Locale.US);
+		messages = ResourceBundle.getBundle(packagePath + "/messages",
+				Locale.US);
 	}
-	
+
 	/**
 	 * Get singleton instance of ResourceLoader
+	 * 
 	 * @return instance of ResourceLoader
 	 */
-	public static ResourceLoader getInstance(){
-		if(resourceLoader == null){
+	public static ResourceLoader getInstance() {
+		if (resourceLoader == null) {
 			resourceLoader = new ResourceLoader();
 		}
 		return resourceLoader;
 	}
-	
+
 	/**
 	 * Get the value for a specific key from the messages file
-	 * @param key for which to get the value
+	 * 
+	 * @param key
+	 *            for which to get the value
 	 * @return the value of the key
 	 */
-	public String getResource(String key){
+	public String getResource(String key) {
 		return messages.getString(key);
 	}
 
