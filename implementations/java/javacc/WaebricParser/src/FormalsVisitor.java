ac612e04dae88db94c5d7b7df9d48b349cc1d2db
jmvn.uva <jmvn.uva@370d2b24-1e00-11de-8120-8d224146dbbc>
2009-06-17 20:11:42 +0000

    Changes: - Moved AST mark-up from Formals non-terminal to FormalsVisitor - Moved AST mark-up from Path non-terminal to PathVisitor

--- implementations/java/javacc/WaebricParser/src/FormalsVisitor.java
+++ implementations/java/javacc/WaebricParser/src/FormalsVisitor.java
@@ -2,7 +2,19 @@ public class FormalsVisitor extends WaebricParserVisitorAdapter {
 	public Object visit(ASTFormals node, Object data){
 		addToAST( "formals([" );
 		
-		addToAST( node.image );
+		String[] imageElements = node.image.split(",");
+		int imageElementCount = imageElements.length;
+		
+		String formalsSet = "";
+		for (int imageElementIndex = 0; imageElementIndex < imageElementCount; imageElementIndex++){
+			if (imageElementIndex > 0) {
+				formalsSet += ", ";
+			}
+			
+			formalsSet += "\"" + imageElements[imageElementIndex] + "\"";
+		}
+		
+		addToAST( formalsSet );
 
 	  	addToAST( "])" );
 		return data;
--- implementations/java/javacc/WaebricParser/src/PathVisitor.java
+++ implementations/java/javacc/WaebricParser/src/PathVisitor.java
@@ -1,6 +1,24 @@
 public class PathVisitor extends WaebricParserVisitorAdapter {	
 	public Object visit(ASTPath node, Object data){		
-		addToAST( node.image );
+		String[] imageElements = node.image.split(":");
+		
+		String astSuffix = "";
+		if (imageElements[0].equals("path-dirfile")){
+			addToAST("path(");
+			astSuffix = ")";
+		}
+		
+		int imageElementCount = imageElements.length;
+		String formalsSet = "";
+		for (int imageElementIndex = 1; imageElementIndex < imageElementCount; imageElementIndex++){
+			if (imageElementIndex > 1) {
+				formalsSet += ", ";
+			}
+			
+			formalsSet += "\"" + imageElements[imageElementIndex] + "\"";
+		}
+		
+		addToAST( formalsSet + astSuffix );
 		return data;
 	}
 }
