ee108f6cd50583f7ebc093e659a2a6267e874285
jmvn.uva <jmvn.uva@370d2b24-1e00-11de-8120-8d224146dbbc>
2009-06-19 20:28:48 +0000

    Changes: - Refactored PathVisitor.java - Refactored PredicateVisitor.java - Refactored SiteVisitor.java

--- trunk/implementations/java/javacc/WaebricParser/src/PathVisitor.java
+++ trunk/implementations/java/javacc/WaebricParser/src/PathVisitor.java
@@ -1,6 +1,6 @@
 public class PathVisitor extends WaebricParserVisitorAdapter {	
 	private static final int LABEL_ELEMENT = 0;
-	private static final int START_OF_DATA = 1;
+	private static final int FIRST_DATA_ELEMENT = 1;
 	private static final int DIR_NAME = 1;
 	private static final int FILENAME_ONLY = 2;
 	
@@ -15,13 +15,13 @@ public class PathVisitor extends WaebricParserVisitorAdapter {
 		
 		int imageElementCount = imageElements.length;
 		String formalsSet = EMPTY_STRING;
-		for (int index = START_OF_DATA; index < imageElementCount; index++){
-			if (1 < index) {
+		for ( int index = FIRST_DATA_ELEMENT; index < imageElementCount; index++ ){
+			if ( FIRST_DATA_ELEMENT < index ) {
 				formalsSet += ", ";
 			}
 			
 			boolean hasDirName = FILENAME_ONLY < imageElementCount;
-			if (hasDirName){
+			if ( hasDirName ){
 				imageElements[DIR_NAME] = imageElements[DIR_NAME].substring(0, imageElements[DIR_NAME].length()-1);
 			}
 			
@@ -29,6 +29,7 @@ public class PathVisitor extends WaebricParserVisitorAdapter {
 		}
 		
 		addToAST( formalsSet + astSuffix );
+		
 		return data;
 	}
 }
