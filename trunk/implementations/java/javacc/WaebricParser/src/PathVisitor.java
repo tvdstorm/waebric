4c854c94fc813968dd449dd14c5563104d4c7116
jmvn.uva <jmvn.uva@370d2b24-1e00-11de-8120-8d224146dbbc>
2009-06-19 15:19:54 +0000

    Changes: - Moved mark-up data from the syntactic specification to ModuleIdVisitor. - Moved mark-up data from the syntactic specification to PathVisitor. - Added function String addQuotes(..) to visitor superclass WaebricParserVisitorAdapter.

--- trunk/implementations/java/javacc/WaebricParser/src/PathVisitor.java
+++ trunk/implementations/java/javacc/WaebricParser/src/PathVisitor.java
@@ -1,21 +1,31 @@
 public class PathVisitor extends WaebricParserVisitorAdapter {	
+	private static final int LABEL_ELEMENT = 0;
+	private static final int START_OF_DATA = 1;
+	private static final int DIR_NAME = 1;
+	private static final int FILENAME_ONLY = 2;
+	
 	public Object visit(ASTPath node, Object data){		
-		String[] imageElements = node.image.split(":");
+		String[] imageElements = node.image.split(SPLIT_SEPARATOR);
 		
-		String astSuffix = "";
-		if (imageElements[0].equals("path-dirfile")){
+		String astSuffix = EMPTY_STRING;
+		if (imageElements[LABEL_ELEMENT].equals("path-dirfile")){
 			addToAST("path(");
 			astSuffix = ")";
 		}
 		
 		int imageElementCount = imageElements.length;
-		String formalsSet = "";
-		for (int imageElementIndex = 1; imageElementIndex < imageElementCount; imageElementIndex++){
-			if (imageElementIndex > 1) {
+		String formalsSet = EMPTY_STRING;
+		for (int index = START_OF_DATA; index < imageElementCount; index++){
+			if (1 < index) {
 				formalsSet += ", ";
 			}
 			
-			formalsSet += "\"" + imageElements[imageElementIndex] + "\"";
+			boolean hasDirName = FILENAME_ONLY < imageElementCount;
+			if (hasDirName){
+				imageElements[DIR_NAME] = imageElements[DIR_NAME].substring(0, imageElements[DIR_NAME].length()-1);
+			}
+			
+			formalsSet += addQuotes(imageElements[index]);
 		}
 		
 		addToAST( formalsSet + astSuffix );
