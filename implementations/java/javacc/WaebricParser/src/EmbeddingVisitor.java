8fa7495188e74c28849c2199a71adfa75279c6c3
jmvn.uva <jmvn.uva@370d2b24-1e00-11de-8120-8d224146dbbc>
2009-06-19 19:39:12 +0000

    Changes: - Refactored EmbedVisitor.java - Refactored EmbeddingVisitor.java - Refactored FormalsVisitor.java - Changed SPLIT_SEPARATOR of the Formals() syntax specs from  "," to ":" so it is the same in all visitors.

--- implementations/java/javacc/WaebricParser/src/EmbeddingVisitor.java
+++ implementations/java/javacc/WaebricParser/src/EmbeddingVisitor.java
@@ -1,20 +1,21 @@
 public class EmbeddingVisitor extends WaebricParserVisitorAdapter {
+	private static final int FIRST_CHILD = 0;
+	
 	public Object visit(ASTEmbedding node, Object data){
+		addToAST( "pre(" + addQuotes("\\" + node.image) + ", " );
+
 		int numberOfChildren = node.jjtGetNumChildren();
-		
-		addToAST( "pre(\"\\" + node.image + "\", " );
-		
-		for ( int currentChild = 0; currentChild < numberOfChildren; currentChild++ ) {
-			if (node.jjtGetChild(currentChild).toString().equals("Embed")){
+		for ( int currentChild = FIRST_CHILD; currentChild < numberOfChildren; currentChild++ ) {
+			if (node.jjtGetChild(currentChild).toString().equals(NODE_EMBED) ){
 				EmbedVisitor embedVisitor = new EmbedVisitor();
   				node.jjtGetChild(currentChild).jjtAccept(embedVisitor, null);
-  				addToAST( embedVisitor.getAST());
+  				addToAST( embedVisitor.getAST() );
   			}
 			
-			else if (node.jjtGetChild(currentChild).toString().equals("TextTail")){
+			else if (node.jjtGetChild(currentChild).toString().equals(NODE_TEXTTAIL) ){
 				TextTailVisitor textTailVisitor = new TextTailVisitor();
   				node.jjtGetChild(currentChild).jjtAccept(textTailVisitor, null);
-  				addToAST( textTailVisitor.getAST());
+  				addToAST( textTailVisitor.getAST() );
   			}
 		}
 		
