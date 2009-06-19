7d0dd121a8df1e08844a9e3d7721c0e1f8784b15
jmvn.uva <jmvn.uva@370d2b24-1e00-11de-8120-8d224146dbbc>
2009-06-19 20:20:13 +0000

    Changes: - Refactored ModuleElementVisitor.java - Refactored ModuleIdVisitor.java - Refactored ModulesVisitor.java - Refactored ModuleVisitor.java

--- implementations/java/javacc/WaebricParser/src/ModuleElementVisitor.java
+++ implementations/java/javacc/WaebricParser/src/ModuleElementVisitor.java
@@ -1,32 +1,31 @@
 public class ModuleElementVisitor extends WaebricParserVisitorAdapter {
+	private static final int FIRST_CHILD = 0;
+	
 	public Object visit(ASTModuleElement node, Object data){
+
 		int numChildren = node.jjtGetNumChildren();
-		
-		addToAST( "" );
-		
-	  	for ( int currentChild = 0; currentChild < numChildren; currentChild++ ) {
-  			// if list
-	  		if (currentChild > 0){
+	  	for ( int currentChild = FIRST_CHILD; currentChild < numChildren; currentChild++ ) {
+	  		if ( FIRST_CHILD < currentChild ){
   				addToAST( ", " );
   			}
-	  		if (node.jjtGetChild(currentChild).toString().equals("Import")){
+	  		
+	  		if ( node.jjtGetChild(currentChild).toString().equals(NODE_IMPORT) ){
 	  			ImportVisitor importVisitor = new ImportVisitor();
 	  			node.jjtGetChild(currentChild).jjtAccept(importVisitor, null);
 	  			addToAST( importVisitor.getAST() );
 	  		} 
-	  		else if (node.jjtGetChild(currentChild).toString().equals("Site")){
+	  		else if ( node.jjtGetChild(currentChild).toString().equals(NODE_SITE) ){
 	  			SiteVisitor siteVisitor = new SiteVisitor();
 	  			node.jjtGetChild(currentChild).jjtAccept(siteVisitor, null);			
 	  			addToAST( siteVisitor.getAST() );
 	  		} 
-	  		else if (node.jjtGetChild(currentChild).toString().equals("FunctionDef")){
+	  		else if ( node.jjtGetChild(currentChild).toString().equals(NODE_FUNCTIONDEF) ){
 	  			FunctionDefVisitor functionDefVisitor = new FunctionDefVisitor();
 	  			node.jjtGetChild(currentChild).jjtAccept(functionDefVisitor, null);			
 	  			addToAST( functionDefVisitor.getAST() );
 	  		}
 	    }
 	  	
-	  	addToAST( "" );
 		return data;
 	}
 }
