ac9238c549b64ce45e9b492f756c1324b61cd8a1
jmvn.uva <jmvn.uva@370d2b24-1e00-11de-8120-8d224146dbbc>
2009-06-19 13:39:41 +0000

    Changes: - Modified TextTailVisitor by adding the reconstructText method to both mid and post text components. Effects can be observer by parsing the home.wae example. With this, and the previous revision.

--- trunk/implementations/java/javacc/WaebricParser/src/TextTailVisitor.java
+++ trunk/implementations/java/javacc/WaebricParser/src/TextTailVisitor.java
@@ -1,16 +1,37 @@
 public class TextTailVisitor extends WaebricParserVisitorAdapter {
+	private static final int LABEL_ELEMENT = 0;
+	private static final int FIRST_DATA_ELEMENT = 1;
+	
 	public Object visit(ASTTextTail node, Object data){
-		String[] dataFromJjt = node.image.split(":");
-		if (dataFromJjt[0].equals("post")){
-			addToAST( "), " + dataFromJjt[0] + "(\"" + dataFromJjt[1].substring(0,dataFromJjt[1].length() -1) + "\\\"\")" );			
-		} else 
-	    if (dataFromJjt[0].equals("mid")){
-			addToAST( "), " + dataFromJjt[0] + "(\"" + dataFromJjt[1] + "\", " );
+		String imageData = node.image;
+		if(lastCharacter(node.image).equals("\"")){
+			// if the last character is a "\"", remove it.
+			imageData = imageData.substring(0, imageData.length() -1);
 		}
-
+		String[] dataFromJjt = imageData.split(":");
 		
+		if (dataFromJjt[LABEL_ELEMENT].equals("post")){
+			// Reconstruction required to counter the effects of splitting when the data contains a colon.
+			String text = reconstructText(dataFromJjt, FIRST_DATA_ELEMENT, SPLIT_SEPARATOR);
+			if(lastCharacter(node.image).equals(SPLIT_SEPARATOR)){
+				text += SPLIT_SEPARATOR;
+			}
+			
+			addToAST( "), " + dataFromJjt[LABEL_ELEMENT] + "(\"" + text + "\\\"\")");			
+		} 
+		
+		else if (dataFromJjt[LABEL_ELEMENT].equals("mid")){
+			// Reconstruction required to counter the effects of splitting when the data contains a colon.
+			String text = reconstructText(dataFromJjt, FIRST_DATA_ELEMENT, SPLIT_SEPARATOR);
+			if(lastCharacter(node.image).equals(SPLIT_SEPARATOR)){
+				text += SPLIT_SEPARATOR;
+			}
+	    	
+	    	addToAST( "), " + dataFromJjt[LABEL_ELEMENT] + "(\"" + text + "\", " );
+		}
+	
+		// Visit the children of TextTail
 		int numberOfChildren = node.jjtGetNumChildren();
-		
 		for ( int currentChild = 0; currentChild < numberOfChildren; currentChild++ ) {
 			if (node.jjtGetChild(currentChild).toString().equals("Embed")){
 				EmbedVisitor embedVisitor = new EmbedVisitor();
