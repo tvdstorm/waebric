ff44ff97164a75b72053c4dc95f62dcd4dfe376a
jmvn.uva <jmvn.uva@370d2b24-1e00-11de-8120-8d224146dbbc>
2009-06-19 18:23:17 +0000

    Changes: - Refactored DesignatorVisitor.java - Refactored DotIdConVisitor.java - Refactored AttributeVisitor.java

--- trunk/implementations/java/javacc/WaebricParser/src/AttributeVisitor.java
+++ trunk/implementations/java/javacc/WaebricParser/src/AttributeVisitor.java
@@ -1,19 +1,30 @@
 public class AttributeVisitor extends WaebricParserVisitorAdapter {
+	private static final int LABEL_ELEMENT = 0;
+	private static final int FIRST_DATA_ELEMENT = 1;
+	
+	private static final String CHOICE_ID = "id";
+	private static final String CHOICE_CLASS = "class";
+	private static final String CHOICE_NAME = "name";
+	private static final String CHOICE_TYPE = "type";
+	private static final String CHOICE_WIDTH_HEIGHT = "width-height";
+	private static final String CHOICE_HEIGHT = "height";
+	
 	public Object visit(ASTAttribute node, Object data){
 		
 		// Make a choice between kinds
-		String[] dataFromJJT = node.image.split(":");
+		String[] dataFromJJT = node.image.split(SPLIT_SEPARATOR);
 		
-		if(	dataFromJJT[0].equals("id") ||
-			dataFromJJT[0].equals("class") ||
-			dataFromJJT[0].equals("name") ||
-			dataFromJJT[0].equals("type")) {
-				addToAST( dataFromJJT[0] + "(\"" +  safeGetStr(dataFromJJT, 1) + "\")" );
+		if(	dataFromJJT[LABEL_ELEMENT].equals(CHOICE_ID) ||
+			dataFromJJT[LABEL_ELEMENT].equals(CHOICE_CLASS) ||
+			dataFromJJT[LABEL_ELEMENT].equals(CHOICE_NAME) ||
+			dataFromJJT[LABEL_ELEMENT].equals(CHOICE_TYPE)) {
+				addToAST( dataFromJJT[LABEL_ELEMENT] + "(" + 
+					addQuotes( safeGetStr(dataFromJJT, FIRST_DATA_ELEMENT) ) + ")" );
 		}
 		
-		else if(dataFromJJT[0].equals("width-height") ||
-				dataFromJJT[0].equals("height")){
-			addToAST( dataFromJJT[0] + "(" + safeGetStr(dataFromJJT, 1) + ")" );
+		else if(dataFromJJT[LABEL_ELEMENT].equals(CHOICE_WIDTH_HEIGHT) ||
+				dataFromJJT[LABEL_ELEMENT].equals(CHOICE_HEIGHT)){
+			addToAST( dataFromJJT[LABEL_ELEMENT] + "(" + safeGetStr(dataFromJJT, FIRST_DATA_ELEMENT) + ")" );
 		}
 
 		return data;
