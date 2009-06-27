4016474232472b68a10d72588185b50bc876f19d
allarddijk@hotmail.com <allarddijk@hotmail.com@370d2b24-1e00-11de-8120-8d224146dbbc>
2009-06-27 20:50:45 +0000

    Total Statement rewrite, IDCONDESIGNATOR verwijderd. stapje terug gedaan, zodat we weer 3 stappen vooruit kunnen... slaagt nu voor de simpele tests maar nog niet voor de grote files. TwoListTypes AST toegevoegd

---
+++ trunk/implementations/java/beaver/waebric/ast/TwoListTypes.java
@@ -0,0 +1,17 @@
+package waebric.ast;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public abstract class TwoListTypes extends Cons
+{
+	public final ArrayList l;
+	public final ArrayList r;
+	
+	protected TwoListTypes(ArrayList left,ArrayList right)
+	{
+		super();
+		l = left;
+		r = right;
+	}
+}
