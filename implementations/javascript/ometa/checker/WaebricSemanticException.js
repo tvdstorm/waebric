1aab75451219a4ab3586dd5149dd92fbc010b358
djnickolas1@gmail.com <djnickolas1@gmail.com@370d2b24-1e00-11de-8120-8d224146dbbc>
2009-07-06 22:06:06 +0000

    Refactoring WaebricSemanticValidator and WaebricSemanticCollector Additional waebric test files

---
+++ implementations/javascript/ometa/checker/WaebricSemanticException.js
@@ -0,0 +1,53 @@
+/** Semantic Exception class 
+ * 
+ * @param {String} The exception message
+ * @return {Error} An error exception
+ */
+function SemanticException(exceptionMessage){
+	return new Error(exceptionMessage);
+}
+
+/** Semantic Exception class for non existing modules
+ * 
+ * @param {String} The path of the non existing module
+ * @return {SemanticException} A Semantic Exception
+ */
+function NonExistingModuleException(path){
+	return new SemanticException("The dependency " + path + " was not found on the filesystem.");
+}
+
+/** Semantic Exception class for undefined functions
+ * 
+ * @param {Designator} The function call for which no function definition exists
+ * @return {SemanticException} A Semantic Exception
+ */
+function UndefinedFunctionException(functionCall){
+	return new SemanticException("The function " + functionCall + " was not found in any of the loaded modules, nor is the functionname part of XHTML");
+}
+
+/** Semantic Exception class for duplicate functions
+ * 
+ * @param {FunctionDefinition} The function definition 
+ * @return {SemanticException} A Semantic Exception
+ */
+function DuplicateDefinitionException(func){
+	return new SemanticException("Duplicate function found: " + func);
+}
+
+/** Semantic Exception class for functions calls with incorrect arguments 
+ * 
+ * @param {Designator} The function call with the incorrect number of arguments
+ * @return {SemanticException} A Semantic Exception
+ */
+function IncorrectArgumentsException(functionCall){
+	return new SemanticException("The functioncall " + functionCall + " has an incorrect number of arguments. ")
+}
+
+/** Semantic Exception class for undefined variables
+ * 
+ * @param {Object} exception
+ * @return {SemanticException} A Semantic Exception
+ */
+function UndefinedVariableException(exception){
+	return new SemanticException("Variable not declared.")
+}
