95142b06e0da60da50e17ea834301830cb168026
djnickolas1@gmail.com <djnickolas1@gmail.com@370d2b24-1e00-11de-8120-8d224146dbbc>
2009-07-22 23:10:05 +0000

    Support for YIELD statement

--- implementations/javascript/ometa/javascript/interpreter/WaebricInterpreter.js
+++ implementations/javascript/ometa/javascript/interpreter/WaebricInterpreter.js
@@ -1,20 +1,71 @@
 /**
- * Interprete the module and converts it to HTML
+ * Interprete the module and returns the HTML output
  */
 function WaebricInterpreter(){
 	
 }
 
+/**
+ * Returns the HTML output for the main function and the site mappings
+ * 
+ * @param {Array} an array of WaebricEnvironments
+ */
 WaebricInterpreter.interpreteAll = function(module){
-	//Setup environment for logging exceptions and storing functions, 
-	//variables and dependencies
-	var env = new WaebricEnvironment();
+	var output = new Array();	
 	
-	//Start semantic validation
-	var document = new DOM();
-	var visitor = new WaebricInterpreterVisitor();
-	module.accept(visitor.getModuleVisitor(env, document));
+	var envMainDocument = getEvnMainDocument(module);
+	if (envMainDocument.path != '') {
+		output.push(envMainDocument);
+	}
 	
-	//Return DOM
-	return document;
+	var outputSiteMappings = getSiteMappings(module);
+	output = output.concat(outputSiteMappings);
+	return output;
+}
+
+/**
+ * Returns the HTML output for the main function
+ * 
+ * @param {Object} module
+ */
+function getEvnMainDocument(module){
+	//Visit module (preprocessing)
+	var visitor = new WaebricInterpreterVisitor()
+	var environment = new WaebricEnvironment();
+	var document = new DOM(); 
+	module.accept(visitor.getModuleVisitor(environment, document));	
+	
+	//Visit main function and write HTML output to document
+	//Exceptions are logged in the environment	
+	module.accept(visitor.getMainVisitor(environment, document));
+	environment.document = document;
+	
+	//Returns the environment and the document
+	return environment;
+}
+
+/**
+ * Returns the HTML output for the site mappings function
+ * 
+ * @param {Array} an array of WaebricEnvironments
+ */
+function getSiteMappings(module){
+	var siteEnvironments = new Array();
+	for (var i = 0; i < module.site.mappings.length; i++) {
+		//Visit module (preprocessing)
+		var visitor = new WaebricInterpreterVisitor();
+		var environment = new WaebricEnvironment();
+		var document = new DOM(); 
+		module.accept(visitor.getModuleVisitor(environment, document));		
+		
+		//Visit main function and write HTML output to document
+		//Exceptions are logged in the environment
+		var mapping = module.site.mappings[i]
+		mapping.accept(visitor.getMappingVisitor(environment, document));
+		environment.document = document;
+		
+		//Returns the environment and the document
+		siteEnvironments.push(environment);
+	}
+	return siteEnvironments;
 }
