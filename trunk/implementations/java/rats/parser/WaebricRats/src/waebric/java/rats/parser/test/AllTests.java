d6fd01aa824c245f79c533fd72a1ab3a5471284a
tvdstorm <tvdstorm@370d2b24-1e00-11de-8120-8d224146dbbc>
2009-07-31 14:12:56 +0000

    Slight refactoring of tests.

---
+++ trunk/implementations/java/rats/parser/WaebricRats/src/waebric/java/rats/parser/test/AllTests.java
@@ -0,0 +1,15 @@
+package waebric.java.rats.parser.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	TestExpressions.class,
+	TestStatements.class,
+	TestModules.class,
+	TestDemos.class
+        })
+public class AllTests {
+
+}
--- trunk/implementations/java/rats/parser/WaebricRats/src/waebric/java/rats/parser/test/TestExpressions.java
+++ trunk/implementations/java/rats/parser/WaebricRats/src/waebric/java/rats/parser/test/TestExpressions.java
@@ -11,14 +11,9 @@ public class TestExpressions extends RatsTesting {
 	}
 
 	@Test
-	public void numberZero() {
-		testExp("0");
-	}
-	
-
-	@Test
-	public void number() {
+	public void numbers() {
 		testExp("1234");
+		testExp("0");
 	}
 
 
@@ -45,119 +40,41 @@ public class TestExpressions extends RatsTesting {
 	@Test
 	public void addition() {
 		testExp("x + x");
-	}
-
-	@Test
-	public void additionLeft() {
 		testExp("x + x + x");
-	}
-
-	@Test
-	public void additionWithDot() {
 		testExp("x + x.a");
 	}
 	
 	@Test
-	public void stringLiteralEmpty() {
+	public void strings() {
 		testExp("\"\"");
-	}
-
-	@Test
-	public void stringLiteralSingle() {
 		testExp("\"a\"");
-	}
-	
-	@Test
-	public void stringLiteralMany() {
 		testExp("\"abcd efg\"");
-	}
-	
-	@Test 
-	public void stringLiteralEscapes() {
 		testExp("\"\\n\\r\\t\\\\\"");
-	}
-
-	@Test 
-	public void stringLiteralNewline() {
 		testExp("\"\n\t\"");
-	}
-
-	@Test 
-	public void stringLiteralDQuoteSingle() {
 		testExp("\"\\\"\"");
-	}
-
-	@Test 
-	public void stringLiteralDQuote() {
 		testExp("\"\\\"abc\"");
 	}
 
 	@Test 
-	public void recordEmpty() {
+	public void records() {
 		testExp("{ }");
-	}
-
-	@Test 
-	public void recordEmptyNoWs() {
 		testExp("{}");
-	}
-
-	@Test 
-	public void recordSingle() {
 		testExp("{a: 3}");
-	}
-	
-	@Test 
-	public void recordMany() {
 		testExp("{a: 123, b: 345}");
-	}
-	
-	@Test 
-	public void listEmpty() {
-		testExp("[ ]");
-	}
-	
-	@Test 
-	public void listEmptyNoWs() {
-		testExp("[]");
-	}
-
-	@Test 
-	public void listSingle() {
-		testExp("[a]");
-	}
-	
-	
-	@Test 
-	public void listMany() {
-		testExp("[a, b, c, 123]");
-	}
-
-	@Test 
-	public void listNestingDirectEmpty() {
-		testExp("[[]]");
-	}
-
-	@Test 
-	public void listNesting() {
-		testExp("[a, [b, c], a]");
-	}
-
-	@Test 
-	public void listNestingInject() {
-		testExp("[a, [b, c], [[[123]]]]");
-	}
-
-	@Test 
-	public void recordNesting() {
 		testExp("{a: {b: 123, c: {k: 345}}}");
 	}
 	
 	@Test 
-	public void variable() {
-		testExp("a");
+	public void lists() {
+		testExp("[ ]");
+		testExp("[]");
+		testExp("[a]");
+		testExp("[a, b, c, 123]");
+		testExp("[[]]");
+		testExp("[a, [b, c], a]");
+		testExp("[a, [b, c], [[[123]]]]");
 	}
-	
+
 	
 	
 }
