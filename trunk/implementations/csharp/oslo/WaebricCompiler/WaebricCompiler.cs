b0abed27f733d17aeb1ea2c5b0bd362d390f643e
jeroenvanlieshout <jeroenvanlieshout@370d2b24-1e00-11de-8120-8d224146dbbc>
2009-08-07 11:31:29 +0000

    Created initial projects according to names used in vanilla project. Parsing already works when right files are included and compiled.

---
+++ trunk/implementations/csharp/oslo/WaebricCompiler/WaebricCompiler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.M;
+using System.Dataflow;
+
+namespace WaebricCompiler
+{
+    public class WaebricCompiler
+    {
+        #region Private Members
+
+        private static String Path; //Path of file to compile
+
+        #endregion
+
+        #region Public Methods
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Waebric Compiler/Interpreter v1.0");
+            Console.WriteLine("--------Builded with Oslo--------");
+            Console.WriteLine("");
+
+            if (args.Length == 1)
+            {   //There is one file specified. 
+                Path = args[0];
+            }
+            else
+            {
+                Console.WriteLine("WeabricCompiler: no input file specified.");
+                return;
+            }
+
+            //Lets initialize the parser
+            
+            WaebricParser parser = new WaebricParser(Path);
+        }
+
+        #endregion
+    }
+}
