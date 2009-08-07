5e3878883212036a5b026e219a46d98417f36aa6
jeroenvanlieshout <jeroenvanlieshout@370d2b24-1e00-11de-8120-8d224146dbbc>
2009-08-07 11:31:29 +0000

    Created initial projects according to names used in vanilla project. Parsing already works when right files are included and compiled.

---
+++ implementations/csharp/oslo/WaebricCompiler/WaebricParser.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.M.Grammar;
+using Microsoft.M;
+using System.Dataflow;
+
+namespace WaebricCompiler
+{
+    /// <summary>
+    /// Class which calls the Oslo Parser and performs parser tasks
+    /// </summary>
+    public class WaebricParser
+    {
+        #region Private Members
+
+        private MImage Grammar;
+        private DynamicParser Parser;
+        private Node Root;
+
+        #endregion
+
+        #region Public Methods
+
+        public WaebricParser(String filename)
+        {
+            //Read grammar and create NodeGraphBuilder
+            Grammar = new MImage("Waebric.mx");
+            Parser = Grammar.ParserFactories["Waebric.Waebric"].Create();
+            Parser.GraphBuilder = new NodeGraphBuilder();
+
+            //Open sourcefile and parse it
+            Root = (Node)Parser.Parse(filename, ErrorReporter.Standard);
+        }
+
+        /// <summary>
+        /// Retrieves AST
+        /// </summary>
+        /// <returns>Root element of AST</returns>
+        public Node GetAST()
+        {
+            return Root;
+        }
+
+        #endregion
+    }
+}
