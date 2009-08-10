2628814bcc87ddd81fc6a0667277b753484ff779
jeroenvanlieshout <jeroenvanlieshout@370d2b24-1e00-11de-8120-8d224146dbbc>
2009-08-10 08:21:53 +0000

    Added build file to make build work outside Visual Studio with additional cleanup and copy actions to let grammar work in application. Also updated readme and problems file.

---
+++ implementations/csharp/oslo/WaebricCompiler/BuildAll.cmd
@@ -0,0 +1,31 @@
+@echo off
+::Checking arguments and setting variable to store proper setting
+if "%1" == "" set version=debug
+if "%1" == "-debug" set version=Debug
+if "%1" == "-release" set version=Release
+
+echo Cleaning up all build directory's...
+if "%version%" == "Debug" rd /s /q bin\debug\
+if "%version%" == "Release" rd /s /q bin\release\
+cd ..\WaebricGrammar
+if "%version%" == "Debug" rd /s /q bin\debug\
+if "%version%" == "Release" rd /s /q bin\release\
+cd ..\Parser
+if "%version%" == "Debug" rd /s /q bin\debug\
+if "%version%" == "Release" rd /s /q bin\release\
+cd ..\Common
+if "%version%" == "Debug" rd /s /q bin\debug\
+if "%version%" == "Release" rd /s /q bin\release\
+cd ..\Checker
+if "%version%" == "Debug" rd /s /q bin\debug\
+if "%version%" == "Release" rd /s /q bin\release\
+:: Add here actions for INTERPRETER!!
+cd ..\WaebricCompiler
+echo Compiling Grammar...
+cd ..\WaebricGrammar
+msbuild WaebricGrammar.mproj /p:Configuration=%version%
+cd ..\WaebricCompiler
+echo Compiling Compiler...
+msbuild WaebricCompiler.csproj /p:Configuration=%version%
+echo Copying compiler grammar to right location
+copy ..\WaebricGrammar\bin\%version%\WaebricGrammar.mx bin\%version%\Waebric.mx
--- implementations/csharp/oslo/docs/Readme.txt
+++ implementations/csharp/oslo/docs/Readme.txt
@@ -12,7 +12,12 @@ Requirements for running tests and build code:
 - SQL Server 2008 Express ("Oslo" prerequisite for repository/quadrant only!) Note: Make sure default instance is selected during install and local user is sql administrator!
 - Microsoft "Oslo" May 2009 CTP release (from http://msdn.com/oslo)
 
-
+Howto build solution:
+---------------------
+1. Start Visual Studio 2008 Command Prompt from startmenu
+2. Navigate to project directory (WaebricCompiler)
+3. Run buildall.cmd with -debug or -release parameter for specific build
 
 Remarks:
 - Make sure your Windows Computer name is UPPERCASE! Otherwise tests would stay pending.
+- Install Oslo on c drive in default folder to make sure project dependency's are ok.
