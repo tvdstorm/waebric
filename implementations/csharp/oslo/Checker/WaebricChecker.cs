07bed4fb5ad19a69b6184b9e9cd63c890014c60c
jeroenvanlieshout <jeroenvanlieshout@370d2b24-1e00-11de-8120-8d224146dbbc>
2009-08-11 10:47:33 +0000

    Implemented first parts of checker. DeclarationChecker checks only for already defined functions now.

--- implementations/csharp/oslo/Checker/WaebricChecker.cs
+++ implementations/csharp/oslo/Checker/WaebricChecker.cs
@@ -2,10 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Dataflow;
 
 namespace Checker
 {
+    /// <summary>
+    /// Class containing checker
+    /// </summary>
     public class WaebricChecker
     {
+        #region Public Methods
+
+        public List<Exception> CheckSyntaxTree(Node tree)
+        {
+            List<Exception> exceptionList = new List<Exception>();
+
+            //Check modules
+            ModuleChecker moduleChecker = new ModuleChecker(exceptionList);
+            moduleChecker.CheckModule(tree);
+
+            //Check declarations
+            DeclarationChecker declarationChecker = new DeclarationChecker(exceptionList);
+            declarationChecker.VisitModule(tree);
+
+            return exceptionList;
+        }
+
+        #endregion
     }
 }
