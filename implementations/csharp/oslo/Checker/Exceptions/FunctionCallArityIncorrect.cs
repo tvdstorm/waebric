47d7c7b04bd1e8d77be3e2aa763c35becf388adf
jeroenvanlieshout <jeroenvanlieshout@370d2b24-1e00-11de-8120-8d224146dbbc>
2009-08-12 07:40:30 +0000

    Added some exception classes for checker

---
+++ implementations/csharp/oslo/Checker/Exceptions/FunctionCallArityIncorrect.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Checker.Exceptions
+{
+    /// <summary>
+    /// Exception when specified arguments at a function call
+    /// are not ok.
+    /// </summary>
+    public class FunctionCallArityIncorrect : Exception
+    {
+        #region Public Methods
+
+        public FunctionCallArityIncorrect(String identifier)
+        {
+            Console.WriteLine("Function " + identifier + " called with wrong number of arguments");
+        }
+
+        #endregion
+    }
+}
---
+++ implementations/csharp/oslo/Checker/Exceptions/UndefinedVariable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Checker.Exceptions
+{
+    /// <summary>
+    /// Exception when non existing variable is called
+    /// </summary>
+    public class UndefinedVariable : Exception
+    {
+        #region Public Methods
+
+        public UndefinedVariable(String identifier)
+        {
+            Console.WriteLine("Variable " + identifier + " used but not declared");
+        }
+
+        #endregion
+    }
+}
