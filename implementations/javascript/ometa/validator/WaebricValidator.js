b05689f1510cdf97eeec324195d0cdb5eec06751
djnickolas1@gmail.com <djnickolas1@gmail.com@370d2b24-1e00-11de-8120-8d224146dbbc>
2009-08-18 19:39:32 +0000

    Added OMeta Validator (extends from OMeta Parser)

---
+++ implementations/javascript/ometa/validator/WaebricValidator.js
@@ -0,0 +1,86 @@
+/** 
+ * OMeta Validator for Waebric language
+ * 
+ * The WaebricOMetaValidator converts a Waebric program into an AST (Module) and then
+ * performs a semantic validation on it.
+ * - Reads in the Waebric program from the filesystem using Rhino/Java
+ * - Parses the tokens to an Abstract Syntax Tree {Module} using OMeta
+ * - Performes semantic validation on the AST using OMeta
+ *
+ * The semantic validation includes:
+ *
+ * - Undefined functions: If for a markup-call f, no function definition can be
+ * 	 found in the current module or one of its transitive imports, and if f is
+ * 	 not a tag defined in the XHTML 1.0 Transitional standard, then this is an error.
+ *
+ * - Undefined variables: If a variable reference x cannot be traced back to an
+ *   enclosing let-definition or function parameter, this is an error.
+ *
+ * - Non-existing module: If for an import directive import m no corresponding file
+ * 	 m.wae can be found, this is an error.
+ *
+ * - Duplicate definitions: Multiple function definitions with the same name are
+ *   disallowed.
+ *
+ * - Arity mismatches: If a function is called with an incorrect numer of arguments
+ *   (as follows from its definition), this is an error.
+ *   
+ * The WaebricOMetaValidator extends the WaebricOMetaParser to recognize the Waebric program 
+ * and is so able to return an AST as well as the semantic exceptions. 
+ * 
+ * Imports in the Waebric program are automatically loaded from the filesystem and parsed 
+ * to an new AST (Module). The new AST is then added to the parent AST (Module) under the
+ * property "dependencies".
+ *   
+ * @author Nickolas Heirbaut [nickolas.heirbaut@dejasmijn.be]
+ */
+function WaebricValidator(){
+	
+	/**
+	 * Validates a Waebric Program and returns an exceptionlist of semantic errors.
+	 * 
+	 * @param {String} path The path of the Waebric program
+	 * @return {Array} An array of semantic errors
+	 */
+	this.parseAndValidate = function(path){
+		var fileExists = (new File(path)).exists();
+		if (fileExists) {			
+			//Setup WaebricOMetaParser for correct Importing
+			WaebricOMetaParser.parentPath = path;
+			WaebricOMetaParser.subParser = WaebricOMetaValidator;
+	
+			//Load the source of the Waebric program
+			var programSource = WaebricFileLoader.loadFile(path);
+			
+			//Parse the source of the Waebric program
+			try {
+				//Parsing + validation
+				var module = WaebricOMetaValidator.matchAll(programSource, 'Module');
+				var exceptions = WaebricOMetaValidator.environment.getExceptions();
+				return new WaebricValidatorResult(module, exceptions);
+			}catch(exception){
+				throw new WaebricParserException("Parsing failed", path, exception);
+			}
+		}else{
+			throw new NonExistingModuleException(path);
+		}	
+	}
+}
+
+/**
+ * Parses and validates a Waebric program
+ * 
+ * @param {Module} module The module to be parsed and validated
+ * @return {WaebricValidatorResult}
+ * @exception {WaebricValidatorException}
+ */
+WaebricValidator.parseAndValidate = function(path){	
+	try {
+		var validator = new WaebricValidator();
+		return validator.parseAndValidate(path);
+	} catch (exception if exception instanceof WaebricValidatorException){
+		throw exception;
+	} catch(exception){
+		throw new WaebricValidatorException("Parsing/validating failed", path, exception);
+	}
+}
---
+++ implementations/javascript/ometa/validator/XHTML.js
@@ -0,0 +1,117 @@
+/**
+ * Holds all valid XHTML tags (copied from www.w3c.com)
+ * 
+ * @author Nickolas Heirbaut [nickolas.heirbaut@dejasmijn.be]
+ */
+function XHTML(){
+	
+}
+
+XHTML.tags = [	
+	'a',
+	'abbr',
+	'acronym',
+	'address',
+	'applet',
+	'area',
+	'b',
+	'base',
+	'basefont',
+	'bdo',
+	'big',
+	'blockquote',
+	'body',
+	'br',
+	'button',
+	'caption',
+	'center',
+	'cite',
+	'code',
+	'col',
+	'colgroup',
+	'dd',
+	'del',
+	'dir',
+	'div',
+	'dfn',
+	'dl',
+	'dt',
+	'em',
+	'fieldset',
+	'font',
+	'form',
+	'frame',
+	'frameset',
+	'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
+	'head',
+	'hr',
+	'html',
+	'i',
+	'iframe',
+	'img',
+	'input',
+	'ins',
+	'isindex',
+	'kbd',
+	'label',
+	'legend',
+	'li',
+	'link',
+	'map',
+	'menu',
+	'meta',
+	'noframes',
+	'noscript',
+	'object',
+	'ol',
+	'optgroup',
+	'option',
+	'p',
+	'param',
+	'pre',
+	'q',
+	's',
+	'samp',
+	'script',
+	'select',
+	'small',
+	'span',
+	'strike',
+	'strong',
+	'style',
+	'sub',
+	'sup',
+	'table',
+	'tbody',
+	'td',
+	'textarea',
+	'tfoot',
+	'th',
+	'thead',
+	'title',
+	'tr',
+	'tt',
+	'u',
+	'ul',
+	'var',
+	'xmp'
+
+
+
+];
+
+/**
+ * Checks whether the input tag is a valid XHTML tag.
+ * 
+ * @param {String} input
+ */
+XHTML.isXHTMLTag = function(input){
+	for (var i = 0; i < XHTML.tags.length; i++) {
+		if (input.toString().toLowerCase() == XHTML.tags[i]) {
+			return true;
+		}
+	}
+	return false;
+}
+
+
