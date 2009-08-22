3b1598ab32a48cb6d4e443232fea439e24e097ed
jeroen.v.schagen <jeroen.v.schagen@370d2b24-1e00-11de-8120-8d224146dbbc>
2009-08-22 09:08:35 +0000

    loader snippets

--- trunk/implementations/java/antlr/src/snippets/LoaderPreamble.java
+++ trunk/implementations/java/antlr/src/snippets/LoaderPreamble.java
@@ -1,4 +1,32 @@
-
-public class ParserPreamble {
-
+package org.cwi.waebric;
+	import antlr.SemanticException;
+	
+public class LoaderPreamble {
+	private List<SemanticException> exceptions = new ArrayList<SemanticException>();
+	private Map<String, function_return> functions = new HashMap<String, function_return>();
+	private List<mapping_return> mappings = new ArrayList<mapping_return>();
+	private List<Integer> yields = new ArrayList<Integer>();
+	
+	/**
+	 * Walk module to retrieve function definitions and mappings
+	 */
+	public List<SemanticException> loadModule() throws RecognitionException {
+		exceptions.clear();
+		functions.clear();
+		mappings.clear();
+		this.module();
+		return exceptions;
+	}
+		
+	public Map<String, function_return> getFunctions() { 
+		return functions;
+	}
+	
+	public List<mapping_return> getMappings() { 
+		return mappings;
+	}
+	
+	public List<Integer> getYields() {
+		return yields;
+	}
 }
