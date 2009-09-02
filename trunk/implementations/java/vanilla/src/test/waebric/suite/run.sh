75e7e51c6c9550f1733ffa7119a1f6438b6f71b8
jeroen.v.schagen <jeroen.v.schagen@370d2b24-1e00-11de-8120-8d224146dbbc>
2009-09-02 09:27:34 +0000

    all functionality tests succeed! off to ANTLR

--- trunk/implementations/java/vanilla/src/test/waebric/suite/run.sh
+++ trunk/implementations/java/vanilla/src/test/waebric/suite/run.sh
@@ -1,6 +1,8 @@
 TESTS=`cat tests.dat`
 
 for t in $TESTS; do
+    cd program
     echo "****************************** Running $t"
-    java -jar ../../../../dist/java-waebric.jar program/$t.wae output/$t.java.html > output/$t.log
+    java -jar ../../../../../dist/java-waebric.jar $t.wae ../output/$t.java.html > ../output/$t.log
+    cd ..
 done
