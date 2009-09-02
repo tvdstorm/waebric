13c06e3a802d95202840f2726a304c6d71fb2d31
jeroenvanlieshout <jeroenvanlieshout@370d2b24-1e00-11de-8120-8d224146dbbc>
2009-09-02 12:39:52 +0000

    Added yieldelement to Oslo implementation for storing SymbolTable and AST subtree for each yieldstatement.

---
+++ implementations/csharp/oslo/Interpreter/YieldElement.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Common;
+using System.Dataflow;
+
+namespace Interpreter
+{
+    /// <summary>
+    /// This elements holds an environment and AST structure for a specific yield statement
+    /// </summary>
+    public class YieldElement
+    {
+        #region Private Members
+
+        private SymbolTable SymbolTable;
+        private Node RootElement;
+
+        #endregion
+
+        #region Public Methods
+
+        public void SetSymbolTable(SymbolTable symbolTable)
+        {
+            SymbolTable = symbolTable;
+        }
+
+        public SymbolTable GetSymbolTable()
+        {
+            return SymbolTable;
+        }
+
+        public void SetRootElement(Node node)
+        {
+            RootElement = node;
+        }
+
+        public Node GetRootElement()
+        {
+            return RootElement;
+        }
+
+        #endregion
+    }
+}
