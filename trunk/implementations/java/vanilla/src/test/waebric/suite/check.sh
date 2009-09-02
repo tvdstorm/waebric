c9e88bde38daea2dbbc526d8962c7110b23e92a0
jeroen.v.schagen <jeroen.v.schagen@370d2b24-1e00-11de-8120-8d224146dbbc>
2009-09-02 07:21:54 +0000

    suite implemented

--- trunk/implementations/java/vanilla/src/test/waebric/suite/check.sh
+++ trunk/implementations/java/vanilla/src/test/waebric/suite/check.sh
@@ -1,7 +1,7 @@
 TESTS=`cat tests.dat`
 
 for t in $TESTS; do
-    cmp output/$t.raw.html output/$t.java.html > $t.diff
+    cmp output/$t.raw.html output/$t.java.html > output/$t.diff
 	if [ $? -gt 0 ]; then 
 		echo "$t fails."
 	fi
--- trunk/implementations/java/vanilla/src/test/waebric/suite/run.sh
+++ trunk/implementations/java/vanilla/src/test/waebric/suite/run.sh
@@ -2,5 +2,5 @@ TESTS=`cat tests.dat`
 
 for t in $TESTS; do
     echo "****************************** Running $t"
-    java -jar ../../../../dist/java-waebric.jar $t.wae output/$t.java.html > log
+    java -jar ../../../../dist/java-waebric.jar program/$t.wae output/$t.java.html > output/$t.log
 done
