d63cbdd50eb014995de588c90ecd73fa3a0009c1
jeroen.v.schagen <jeroen.v.schagen@370d2b24-1e00-11de-8120-8d224146dbbc>
2009-09-07 07:30:30 +0000

    visitors return T

--- implementations/java/vanilla/src/test/waebric/suite/run.sh
+++ implementations/java/vanilla/src/test/waebric/suite/run.sh
@@ -3,6 +3,6 @@ TESTS=`cat tests.dat`
 for t in $TESTS; do
     cd program
     echo "****************************** Running $t"
-    java -jar ../../../../../dist/java-waebric.jar $t.wae ../output/$t.java.html > ../output/$t.log
+    java -jar ../../../../../dist/java-waebric-T.jar $t.wae ../output/$t.java.html > ../output/$t.log
     cd ..
 done
